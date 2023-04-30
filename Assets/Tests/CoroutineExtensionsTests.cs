09da2432668e1ccd1816a6881e563f87e344aff3
Avangardum <yurykuz65@gmail.com>
2023-04-30 15:56:26 +0300

    Added CoroutineExtensions with ToTask method

---
+++ Assets/Tests/CoroutineExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Avangardum.AvangardumUnityUtilityLib.Tests
+{
+    [TestFixture]
+    public class CoroutineExtensionsTests
+    {
+        private int _testInt;
+
+        [Test]
+        public async Task ToTaskConvertsCoroutineToTask()
+        {
+            var task = CoroutineHelper.StartCoroutine(TestCoroutine()).ToTask();
+            Assert.That(_testInt, Is.EqualTo(1));
+            await task;
+            Assert.That(_testInt, Is.EqualTo(2));
+        }
+        
+        private IEnumerator TestCoroutine()
+        {
+            _testInt = 1;
+            yield return new WaitForEndOfFrame();
+            _testInt = 2;
+        }
+    }
+}
